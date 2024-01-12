01d5164c5e2f6c32467d70d1562d6fa2a9439322
jhwan <jhwan.kim95@gmail.com>
2024-01-12 20:40:07 +0900

    update : jhwan êãversion created

--- src/CampManagementApplication.java
+++ src/CampManagementApplication.java
@@ -1,8 +1,6 @@
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Notification
@@ -13,7 +11,7 @@ import java.util.Scanner;
  * 구현에 도움을 주기위한 Base 프로젝트입니다. 자유롭게 이용해주세요!
  */
 
-public class CampManagementApplication1 {
+public class CampManagementApplication {
     // 데이터 저장소
     private static List<Student> studentList = new ArrayList<>();
     private static List<Subject> subjectList;
@@ -143,19 +141,71 @@ public class CampManagementApplication1 {
             System.out.println("수강생 관리 실행 중...");
             System.out.println("1. 수강생 등록");
             System.out.println("2. 수강생 목록 조회");
-            System.out.println("3. 메인 화면 이동");
+            System.out.println("3. 수강생 필수, 선택 과목 선택");
+            System.out.println("4. 메인 화면 이동");
             System.out.print("관리 항목을 선택하세요...");
             int input = sc.nextInt();
 
             switch (input) {
                 case 1 -> createStudent(); // 수강생 등록
                 case 2 -> inquireStudent(); // 수강생 목록 조회
-                case 3 -> flag = false; // 메인 화면 이동
+                case 3 -> chooseSubjects();
+                case 4 -> flag = false; // 메인 화면 이동
                 default -> {
                     System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
                     flag = false;
                 }
             }
+        }
+    }
+
+    private static void chooseSubjects() {
+
+        System.out.println("학생 번호를 입력하세요. \nex) ST1");
+        sc.nextLine(); //버퍼빼는 scanner
+        String stuNum = sc.nextLine();
+        System.out.println("stuNum : "+ stuNum);
+
+        boolean flag = false;
+        Student student = new Student("필요없는가비지객체", "가비지객체");
+        for(Student stu : studentList){
+            if(stu.getStudentId().equals(stuNum)){
+                student = stu;
+                flag = true;
+                break;
+            }
+        }
+
+        // 해당하는 학생이 있을 경우
+        if(flag){
+            System.out.println();
+            System.out.println("과목을 필수 3개, 선택 2개를 선택하십시오. ");
+            for(int i=0; i<subjectList.size(); i++){
+
+                String subName = subjectList.get(i).getSubjectName();
+                String subProperty = subjectList.get(i).getSubjectType();
+                System.out.println(i+"번 | "+ subProperty + " | " +subName);
+            }
+
+            String input = sc.nextLine();
+            StringTokenizer st = new StringTokenizer(input);
+
+            student.addSubject(subjectList.get(Integer.parseInt(st.nextToken())));
+            student.addSubject(subjectList.get(Integer.parseInt(st.nextToken())));
+            student.addSubject(subjectList.get(Integer.parseInt(st.nextToken())));
+            student.addSubject(subjectList.get(Integer.parseInt(st.nextToken())));
+            student.addSubject(subjectList.get(Integer.parseInt(st.nextToken())));
+
+            System.out.println("add 완료되었습니다. !");
+
+            List<Subject> stuSubList = student.getSubjects();
+            stuSubList.forEach(s->{
+                System.out.println(s.getSubjectName());
+            });
+        }
+        // 해당하는 학생이 없을 경우
+        else{
+            System.out.println("해당하는 학생이 없습니다.");
         }
     }
 
@@ -166,95 +216,86 @@ public class CampManagementApplication1 {
 
 
         // 기능 구현 (필수 과목, 선택 과목)
-
+        sc.nextLine();
         String studentName = sc.nextLine();
-        for (Subject sj : subjectList){
-            System.out.println(sj.getSubjectId() + sj.getSubjectName() + sj.getSubjectType());
+        Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName);
+        studentList.add(student);
+
+    }
+
+    // 수강생 목록 조회
+    private static void inquireStudent() {
+        System.out.println("\n수강생 목록을 조회합니다...");
+        // 기능 구현
+        for(Student stu : studentList){
+            System.out.println(stu.getStudentId() + " | " + stu.getStudentName());
         }
-        List<Subject>arrayList = new ArrayList <>();    // subject에서 선언
-//        arrayList.add(new Subject());   // 입력받은 값을 비교해서 같은 값이면 입력받은 값을 add 없으면 다시 입력
+        System.out.println("\n수강생 목록 조회 성공!");
+    }
 
-//        // hashMap으로 불러오기
-//            Subject subject = new Subject();
-//        // 1 , 장원녕 , 자바 , 1, 필수 (과목이 정해져있던경우 이거 필요없음)
-//        // seq => 너무 어려움, 불필요한 코드 (자동증가)
-//            // 수강생 인스턴스 생성 예시 코드
-//            Student student = new Student(sequence(INDEX_TYPE_STUDENT),장원녕,subject.getSubjectId(),subject.getSubjectType(),subject.getSubjectName());
-////            studentList.add(student);
-        System.out.println("d");
-            System.out.println("수강생 등록 성공!\n");
-        }
+    private static void displayScoreView() {
+        boolean flag = true;
+        while (flag) {
+            System.out.println("==================================");
+            System.out.println("점수 관리 실행 중...");
+            System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
+            System.out.println("2. 수강생의 과목별 회차 점수 수정");
+            System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
+            System.out.println("4. 메인 화면 이동");
+            System.out.print("관리 항목을 선택하세요...");
+            int input = sc.nextInt();
 
-        // 수강생 목록 조회
-        private static void inquireStudent () {
-            System.out.println("\n수강생 목록을 조회합니다...");
-            // 기능 구현
-            System.out.println("\n수강생 목록 조회 성공!");
-        }
-
-        private static void displayScoreView () {
-            boolean flag = true;
-            while (flag) {
-                System.out.println("==================================");
-                System.out.println("점수 관리 실행 중...");
-                System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
-                System.out.println("2. 수강생의 과목별 회차 점수 수정");
-                System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
-                System.out.println("4. 메인 화면 이동");
-                System.out.print("관리 항목을 선택하세요...");
-                int input = sc.nextInt();
-
-                switch (input) {
-                    case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
-                    case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
-                    case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
-                    case 4 -> flag = false; // 메인 화면 이동
-                    default -> {
-                        System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
-                        flag = false;
-                    }
+            switch (input) {
+                case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
+                case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
+                case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
+                case 4 -> flag = false; // 메인 화면 이동
+                default -> {
+                    System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
+                    flag = false;
                 }
             }
         }
-
-        private static String getStudentId () {
-            System.out.print("\n관리할 수강생의 번호를 입력하시오...");
-            return sc.next();
-        }
-
-        // 수강생의 과목별 시험 회차 및 점수 등록
-        private static void createScore () {
-            String studentId = getStudentId(); // 관리할 수강생 고유 번호
-            System.out.println("시험 점수를 등록합니다...");
-            // 기능 구현
-            System.out.println("\n점수 등록 성공!");
-        }
-
-        // 수강생의 과목별 회차 점수 수정
-        private static void updateRoundScoreBySubject () {
-            String studentId = getStudentId(); // 관리할 수강생 고유 번호
-            // 기능 구현 (수정할 과목 및 회차, 점수)
-            System.out.println("시험 점수를 수정합니다...");
-            // 기능 구현
-            System.out.println("\n점수 수정 성공!");
-        }
-
-        // 수강생의 특정 과목 회차별 등급 조회
-        private static void inquireRoundGradeBySubject () {
-            String studentId = getStudentId(); // 관리할 수강생 고유 번호
-            // 기능 구현 (조회할 특정 과목)
-            System.out.println("회차별 등급을 조회합니다...");
-            // 기능 구현
-            System.out.println("\n등급 조회 성공!");
-        }
-
     }
-    // 캠프 매니저는 수강생을 등록 및 관리할 수 있다
 
-    //  수강생, 과목, 필수여부
-    // 학생번호 key값(UUID)으로 두고 ArrayList 하나 생성
-    // students 와 subject는 상속관계아님
-    // HashMap<studentName, ArrayList<Subject>>
+    private static String getStudentId() {
+        System.out.print("\n관리할 수강생의 번호를 입력하시오...");
+        return sc.next();
+    }
+
+    // 수강생의 과목별 시험 회차 및 점수 등록
+    private static void createScore() {
+        String studentId = getStudentId(); // 관리할 수강생 고유 번호
+        System.out.println("시험 점수를 등록합니다...");
+        // 기능 구현
+        System.out.println("\n점수 등록 성공!");
+    }
+
+    // 수강생의 과목별 회차 점수 수정
+    private static void updateRoundScoreBySubject() {
+        String studentId = getStudentId(); // 관리할 수강생 고유 번호
+        // 기능 구현 (수정할 과목 및 회차, 점수)
+        System.out.println("시험 점수를 수정합니다...");
+        // 기능 구현
+        System.out.println("\n점수 수정 성공!");
+    }
+
+    // 수강생의 특정 과목 회차별 등급 조회
+    private static void inquireRoundGradeBySubject() {
+        String studentId = getStudentId(); // 관리할 수강생 고유 번호
+        // 기능 구현 (조회할 특정 과목)
+        System.out.println("회차별 등급을 조회합니다...");
+        // 기능 구현
+        System.out.println("\n등급 조회 성공!");
+    }
+
+}
+// 캠프 매니저는 수강생을 등록 및 관리할 수 있다
+
+//  수강생, 과목, 필수여부
+// 학생번호 key값(UUID)으로 두고 ArrayList 하나 생성
+// students 와 subject는 상속관계아님
+// HashMap<studentName, ArrayList<Subject>>
 // 수강생 등록시 이름만 입력 => 과목 선택 list of 로 서브젝트 정보 보여줌
 // 가장 상위개념은 student => 과목 < 점수 (상속)
 // 그후 hashMap에 put
--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
     private String studentId;
     private String studentName;
 
+    private List<Subject> subjects;
+
     public Student(String seq, String studentName) {
         this.studentId = seq;
         this.studentName = studentName;
+        this.subjects = new ArrayList<>();
     }
 
     // Getter
@@ -17,4 +22,15 @@ public class Student {
         return studentName;
     }
 
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> setListSubject) {
+        this.subjects = setListSubject;
+    }
+
+    public void addSubject(Subject subject){
+        this.subjects.add(subject);
+    }
 }
